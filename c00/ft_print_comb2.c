e7aa487dc4e42c458934a0e85d7e1a3e515c5c79
Abdelaziz <abdobm2811@gmail.com>
2025-03-18 11:09:58 -0400

    add ft_print_comb2.c

--- c00/ft_print_comb2.c
+++ c00/ft_print_comb2.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 void ft_print_comb2()
-
+{
 	char c1, c2, c3, c4;
 	
-	for (int i = 48; i <= 57; i++)
+	for (int i = 0; i <= 98; i++)
 	{
-		for (int j = 48; j <= 57; j++)
+		for (int j = i + 1; j <= 99; j++)
 		{
 			c1 = (i / 10) + '0';
 			c2 = (i % 10) + '0';
@@ -16,12 +16,12 @@ void ft_print_comb2()
 			write(1, &c1, 1);
 			write(1, &c2, 1);
 			write(1, " ", 1);
-			write(1, %c3, 1);
+			write(1, &c3, 1);
 			write(1, &c4, 1);
 
-			if (!(i == 57 && j == 57))
+			if (!(i == 98 && j == 99))
 			{
-				write(1, ", ", 1);
+				write(1, ", ", 2);
 			}
 		}
 	}
