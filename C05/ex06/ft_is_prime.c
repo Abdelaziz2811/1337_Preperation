d1f3559c7671b2f14fe92b115cf8f833c43c7677
Abdelaziz <abdobm2811@gmail.com>
2025-04-26 11:24:11 +0100

    C05/ex06/ft_is_prime.c

---
+++ C05/ex06/ft_is_prime.c
@@ -0,0 +1,24 @@
+int ft_sqrt(int nb)
+{
+	if (nb < 0) return 0;
+	if (nb == 0 || nb == 1) return nb;
+
+	int i = 2;
+	while (i * i <= nb)
+	{
+		if (i * i == nb) return i;
+		i++;
+	}
+	return i - 1;
+}
+
+int ft_is_prime(int nb)
+{
+	if (nb <= 1 ) return 0;
+	int i, nb_sqrt = ft_sqrt(nb);
+	for (i = 2; i <= nb_sqrt; i++)
+	{
+		if (nb % i == 0) return 0;
+	}
+	return 1;
+}
