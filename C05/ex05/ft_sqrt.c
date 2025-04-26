a3b3345c9ef5e61896025d6ce3d56de5096983b1
Abdelaziz <abdobm2811@gmail.com>
2025-04-26 10:01:39 +0100

    C05/ex05/ft_sqrt.c

---
+++ C05/ex05/ft_sqrt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int ft_sqrt(int nb)
+{
+	if (nb < 0) return 0;
+	if (nb == 0 || nb == 1) return nb;
+
+	int i = 2;
+	while (i * i <= nb)
+	{
+		if (i * i == nb) return i;
+		i++;
+	}
+	return 0;
+}
+
+int main()
+{
+	printf("%d", ft_sqrt(8));
+	return 0;
+}
