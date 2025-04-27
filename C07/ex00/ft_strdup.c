2783617d6aa9623f40c550bb254a3e77f3506946
Abdelaziz <abdobm2811@gmail.com>
2025-04-27 18:29:56 +0100

    C07/ex00/ft_strdup.c

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+char *ft_strdup(char *src)
+{
+	int length = 0;
+	while (src[length])
+		length++;
+
+	char *copy = malloc(length + 1);
+
+	if (copy != NULL)
+	{
+		int i = 0;
+		while (i < length)
+		{
+			copy[i] = src[i];
+			i++;
+		}
+		copy[i] = '\0';
+	}
+	
+	return copy;
+}
