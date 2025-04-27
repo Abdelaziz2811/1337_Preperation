5fd2bab136c8dbcec53a08ba780174088e50b3d1
Abdelaziz <abdobm2811@gmail.com>
2025-04-27 17:51:11 +0100

    C06/ex01/ft_print_params.c

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	if (argc >= 2)
+	{
+		int i = 1, j = 0;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				write(1, &argv[i][j], 1);
+				j++;
+			}
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return 0;
+}
