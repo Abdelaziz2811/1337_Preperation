90c7f52721eb7aef3c1f93e06ec803f7126c1fd3
Abdelaziz <abdobm2811@gmail.com>
2025-05-20 10:07:57 +0100

    Exams/LVL4/expand_str

---
+++ Exams/LVL4/expand_str/expand_str.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+int main(int argc, char  **argv)
+{
+	if (argc == 2)
+	{
+		int i = 0;
+
+		while (argv[1][i] == ' ' || argv[1][i] ==  '\t')
+			i++;
+
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+			{
+				while (argv[1][i] == ' ' || argv[1][i] == '\t')
+					i++;
+				
+				if (argv[1][i])
+					write(1, "   ", 3);
+			}
+
+			if (argv[1][i])
+			{
+				write(1, &argv[1][i], 1);
+				i++;
+			}
+		}
+	}
+	write(1, "\n", 1);
+	return 0;
+}
